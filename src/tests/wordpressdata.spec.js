2c1bb4025c4c0f4d418b9ebaf008b8fa11b8ce47
trentonlawton <trentonlawton@gmail.com>
2016-12-10 22:34:09 -0800

    Set up html reporter

--- src/tests/wordpressdata.spec.js
+++ src/tests/wordpressdata.spec.js
@@ -1,4 +1,7 @@
 describe("For consuming wordpress endpoind",function(){
+  $.getJSON("http://www.usernameisnull.com/data/wp-json/wp/v2/posts/?_embed",function(data){
+    console.log(data);
+  })
 it("should have a json response",function(){
 expect("foo").toEqual("bar");
 })
